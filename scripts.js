81560fd2fa488157175305e44a87bce4f0d6002a
TheRagingAsura <undertakerryu@gmail.com>
2022-02-16 16:38:10 -0500

    0

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
-class Name {
-    speak() {
-        console.log("bark bark")
+class MyClassName {
+    constructor(namer) {
+        this.namer = namer;
     }
+
+    greeting() {
+        console.log(`Hello from ${this.namer}`)
+    }
+
+    setName(namer) {
+        this.namer = namer;
+    }
+
 }
 
 
-const thing = new Name();
-
-thing.speak()
+const nikolai = new MyClassName("Nikolai");
+nikolai.greeting();
+nikolai.setName("Apollo");
+nikolai.greeting();
