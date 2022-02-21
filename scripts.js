7408a2bf49cf5e52c7065200566c4f9500982f85
Nikolai <undertakerryu@gmail.com>
2022-02-21 03:26:37 -0500

    grid and selector updates

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
-document.getElementsByClassName('test').onclick = function ButtonClick() {
-
-    document.getElementsByClassName('test').innerHTML = "tests";
+document.getElementById('Randomizer').onclick = function ButtonClick() {
+let RNG = Math.round(Math.random() *100)
     
-
  }
+
+
+// Pulls in sprite from JSON file and replaces src in html
+function getSprite() {
+
+let Sprite = "/images/pokeball-png-photos.png";
+document.getElementById("PokemonSprite").src = Sprite;
+}
+getSprite()
+
