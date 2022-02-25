b1c874b6463d3c5b458908f9a23732c6dcc69c52
Nikolai <undertakerryu@gmail.com>
2022-02-25 01:24:00 -0500

    API pulled onclick with random number

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 document.getElementById('Randomizer').onclick = function ButtonClick() {
-let RNG = Math.round(Math.random() *100)
-    
- }
+let RNG = Math.round(Math.random() *100);
+let pokeAPI = "https://pokeapi.co/api/v2/pokemon/";
+let randomPokemonPage = pokeAPI + RNG;
+
+fetch(randomPokemonPage);
+console.log(randomPokemonPage);
+
+
+
+}
 
 
 // Pulls in sprite from JSON file and replaces src in html
@@ -11,4 +18,10 @@ let Sprite = "/images/pokeball-png-photos.png";
 document.getElementById("PokemonSprite").src = Sprite;
 }
 getSprite()
+
+
+
+let tester = "test2" 
+document.getElementById("MoveOneHeader").innerText = tester;
+
 
