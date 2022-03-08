be5fa65c2a75b04ad3fa8ee8a69ffa3b825b4791
TheRagingAsura <undertakerryu@gmail.com>
2022-03-08 06:43:31 -0500

    move updates

--- scripts.js
+++ scripts.js
@@ -7,19 +7,47 @@ fetch(randomPokemonPage)
     .then(response => response.json())
     .then(Pokemon => {
         console.log(randomPokemonPage);
-    document.getElementById("PokemonName").innerHTML = Pokemon.name;
+    document.getElementById("PokemonName").innerHTML = Pokemon.name.toUpperCase();
     document.getElementById("PokemonHP").innerHTML = Pokemon.stats[0].base_stat;
     document.getElementById("PokemonSprite").src = Pokemon.sprites.front_default;
+
+    document.getElementById("Move1Header").innerHTML = Pokemon.moves[0].move.name.toUpperCase();
+        fetch(Pokemon.moves[0].move.url)
+        .then(response => response.json())
+        .then(move1 => {
+            console.log(move1);
+        document.getElementById("Move1FlavorText").innerHTML = move1.flavor_text_entries[0].flavor_text
+        })
+
+        document.getElementById("Move2Header").innerHTML = Pokemon.moves[1].move.name.toUpperCase();
+        fetch(Pokemon.moves[1].move.url)
+        .then(response => response.json())
+        .then(move2 => {
+            console.log(move2);
+        document.getElementById("Move2FlavorText").innerHTML = move2.flavor_text_entries[1].flavor_text
+        })
+
+
+    document.getElementById("Move3").innerHTML = Pokemon.moves[2].move.name.toUpperCase();
+
+   
     let test = document.getElementById("lmao");
     test.classList.add(Pokemon.types[0].type.name);
 
         // removes last added class as well as removes DefaultCard
-    test.classList.remove(test.classList.item(1));
+    setTimeout(() => {
+        test.classList.remove(test.classList.item(1));
+        
+    }, 800);
     
 
     })
 
 }
+
+
+
+
 
 
 // Pulls in sprite from JSON file and replaces src in html
